f20ce1b5748863fb1d2c8a7a6582aebe7152a0a1
robwileycodes <robertpwileyjr@gmail.com>
2025-10-06 10:12:33 -0400

    final calculator

--- script.js
+++ script.js
@@ -8,6 +8,10 @@ function clearDisplay(){
     display.value=''
 }
 
+function del() {
+    display.value = display.value.slice(0, -1);
+}
+
 function calculate(){
     try{
     display.value = eval(display.value);
